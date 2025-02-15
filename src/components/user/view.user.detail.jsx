cc80e5d9bf633f95e71285e9cd80c11b624d4873
MSI\Admin <101080058+sydvph19885@users.noreply.github.com>
2025-02-15 22:35:18 +0700

    add display avatar function

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,5 +1,4 @@
 import { Drawer } from "antd";
-import { useEffect, useState } from "react";
 
 const DetailUserModal = (props) => {
   const { isDetailModal, setIsDetailModal, dataDetail, setDataDetail } = props;
@@ -15,10 +14,34 @@ const DetailUserModal = (props) => {
     >
       {dataDetail ? (
         <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              objectFit: "cover",
+            }}
+          >
+            <img
+              style={{ width: "200px", height: "200px" }}
+              src={`${import.meta.env.VITE_BACKEND_URL}//images/avatar/${
+                dataDetail.avatar
+              }`}
+              alt=""
+            />
+          </div>
           <p>ID: {dataDetail._id} </p>
           <p>Full Name: {dataDetail.fullName} </p>
           <p>Email: {dataDetail.email} </p>
           <p>Phone: {dataDetail.phone} </p>
+          <div>
+            <label
+              htmlFor="btnUploadImg"
+              style={{ cursor: "pointer", background: "orange" }}
+            >
+              <h1>Upload Img</h1>
+            </label>
+            <input type="file" hidden id="btnUploadImg" />
+          </div>
         </div>
       ) : (
         <p>Khong co du lieu</p>
